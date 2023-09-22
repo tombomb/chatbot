440b1b381c7c13cf4bda3f5944b1391882f35fc9
Tom McClellan <tombomb318@gmail.com>
2023-09-22 04:45:35 -0400

    Coding is funnnnnnn 	modified:   .dockerignore 	modified:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   public/script.js 	modified:   public/styles.css 	modified:   server.js

--- public/script.js
+++ public/script.js
@@ -2,70 +2,239 @@ const player = document.getElementById('audio-player');
 
 async function sendMessage() {
     const userInput = document.getElementById('user-input');
-    const message = userInput.value;
-    if (!message.trim()) return;  // If the message is just whitespace, don't send it
+    const submitButton = document.querySelector('button');
+    const message = userInput.value.trim();
 
-    // Display the user's message on the chat
+    if (!message) return;
+
+    submitButton.disabled = true;
+    userInput.value = '';
+
     displayMessage(message, 'user');
 
-    // Send the message to the server
-    const response = await fetch('/chat', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ message: message })
-    });
+    try {
+        const response = await fetch('/chat', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ message })
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    // Display the bot's response on the chat
-    if (data.message) {
-        displayMessage(data.message, 'bot', data.audioURL);
-    } else {
-        console.error("No bot response received.");
+        if (data.message) {
+            displayMessage(data.message, 'assistant', data.audioURL);
+            playAudio(data.audioURL);
+        } else {
+            console.error("No bot response received.");
+        }
+    } catch (error) {
+        console.error("Error sending message:", error);
+    } finally {
+        submitButton.disabled = false;
     }
-
-    // Play the audio response
-    if (data.audioURL) {
-        playAudio(data.audioURL);
-    } else {
-        console.error("No audio path received.");
-    }
-
-    // Clear the input field
-    userInput.value = '';
 }
 
 function displayMessage(message, sender, audioURL) {
     const chatHistory = document.getElementById('chat-history');
+    const messageDiv = document.createElement('div');
+    messageDiv.className = `${sender}-message`;
+    messageDiv.textContent = message;
 
-    if (sender === 'bot' && audioURL) {
-        const botContainer = document.createElement('div');
-        botContainer.className = 'bot-container';
-
-        const messageDiv = document.createElement('div');
-        messageDiv.className = 'bot-message';
-        messageDiv.textContent = message;
-        botContainer.appendChild(messageDiv);
-
+    if (sender === 'assistant' && audioURL) {
+        messageDiv.onclick = () => playAudio(audioURL);
         const downloadButton = document.createElement('a');
         downloadButton.href = audioURL;
         downloadButton.download = audioURL.split('/').pop();
-        downloadButton.textContent = 'Download';
-        downloadButton.className = 'download-button';
-        botContainer.appendChild(downloadButton);
+        downloadButton.className = 'btn btn-link download-button';
+        downloadButton.innerHTML = '<i class="fas fa-download"></i>';
 
-        chatHistory.appendChild(botContainer);
+        const botContainer = document.createElement('div');
+        botContainer.className = 'bot-container';
+        botContainer.append(messageDiv, downloadButton);
+        chatHistory.prepend(botContainer); // Change this line
     } else {
-        const messageDiv = document.createElement('div');
-        messageDiv.className = sender === 'user' ? 'user-message' : 'bot-message';
-        messageDiv.textContent = message;
-        chatHistory.appendChild(messageDiv);
+        chatHistory.prepend(messageDiv); // Change this line
     }
 
     chatHistory.scrollTop = chatHistory.scrollHeight;
 }
+
+function displaySingleMessage(message) {
+    const chatHistory = document.getElementById('chat-history');
+    const messageDiv = document.createElement('div');
+    messageDiv.className = `${message.role}-message`;
+    messageDiv.textContent = message.content;
+
+    if (message.role === 'assistant' && message.audioPath) {
+        // Add an onclick event to the bot message div to play the associated audio
+        messageDiv.onclick = function() {
+            playAudio(message.audioPath);
+        };
+
+        const downloadButton = document.createElement('a');
+        downloadButton.href = message.audioPath;
+        downloadButton.download = message.audioPath.split('/').pop();
+        downloadButton.className = 'btn btn-link download-button';
+        downloadButton.innerHTML = '<i class="fas fa-download"></i>';
+
+        const botContainer = document.createElement('div');
+        botContainer.className = 'bot-container';
+        botContainer.appendChild(messageDiv);
+        botContainer.appendChild(downloadButton);
+        chatHistory.prepend(botContainer); // Change this line
+    } else {
+        chatHistory.prepend(messageDiv); // Change this line
+    }
+
+    chatHistory.scrollTop = chatHistory.scrollHeight;
+}
+
+async function loadMessageHistory(filename) {
+    if (!filename) {
+        console.warn("No filename provided. Skipping loading message history.");
+        return;
+    }
+
+    // Clear the chat history display in the DOM
+    const chatHistory = document.getElementById('chat-history');
+    chatHistory.innerHTML = '';
+
+    try {
+        const response = await fetch(`/getHistory/${filename}`);
+        const history = await response.json();
+        for (const message of history) {
+            displaySingleMessage(message);
+        }
+    } catch (error) {
+        console.error("Error loading message history:", error);
+    }
+}
+
+async function loadConversations() {
+    try {
+        const response = await fetch('/getConversations');
+        const conversations = await response.json();
+        const chatList = document.getElementById('chat-list');
+        chatList.innerHTML = ''; // Clear the list
+
+        for (const conversation of conversations) {
+            const listItem = document.createElement('li');
+            listItem.dataset.filename = conversation.filename; // Store filename in dataset for later use
+
+            const nameSpan = document.createElement('span');
+            nameSpan.className = 'conversation-name';
+            nameSpan.textContent = conversation.name || conversation.filename;
+            listItem.appendChild(nameSpan);
+
+            const editIcon = document.createElement('i');
+            editIcon.className = "fas fa-pencil-alt edit-icon";
+            listItem.appendChild(editIcon);
+
+            // Attach click event to load the chat history
+            listItem.onclick = function(event) {
+                if (event.target !== editIcon) { // Ensure we're not clicking the edit icon
+                    loadMessageHistory(listItem.dataset.filename);
+                }
+            };
+
+            editIcon.onclick = function() {
+                triggerEdit(listItem, conversation, editIcon);
+            };
+
+            chatList.appendChild(listItem);
+        }
+    } catch (error) {
+        console.error("Error loading conversations:", error);
+    }
+}
+
+async function saveChanges(input, listItem, conversation, editIcon) {
+    const newName = input.value.trim();
+    if (newName && newName !== conversation.name) {
+        try {
+            const response = await fetch('/updateConversationName', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ filename: conversation.filename, newName })
+            });
+
+            if (response.ok) {
+                // Create a new span with the class .conversation-name
+                const nameSpan = document.createElement('span');
+                nameSpan.className = 'conversation-name';
+                nameSpan.textContent = newName;
+
+                // Clear the listItem and append the new span and editIcon
+                listItem.innerHTML = '';
+                listItem.append(nameSpan, editIcon);
+
+                conversation.name = newName;
+                editIcon.className = "fas fa-pencil-alt edit-icon";
+            } else {
+                console.error('Failed to update the conversation name.');
+            }
+        } catch (error) {
+            console.error("Error updating conversation name:", error);
+        }
+    } else {
+        // Create a new span with the class .conversation-name
+        const nameSpan = document.createElement('span');
+        nameSpan.className = 'conversation-name';
+        nameSpan.textContent = conversation.name || conversation.filename;
+
+        // Clear the listItem and append the new span and editIcon
+        listItem.innerHTML = '';
+        listItem.append(nameSpan, editIcon);
+
+        editIcon.className = "fas fa-pencil-alt edit-icon";
+    }
+    setTimeout(() => {
+        // Reset the edit icon's click event to trigger the edit mode
+        editIcon.onclick = function() {
+            triggerEdit(listItem, conversation, editIcon);
+        };
+    }, 100); // 100ms delay
+}
+
+function triggerEdit(listItem, conversation, editIcon) {
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = conversation.name || conversation.filename;
+
+    input.addEventListener('keypress', async function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            await saveChanges(input, listItem, conversation, editIcon);
+        }
+    });
+
+    input.onblur = async function() {
+        await saveChanges(input, listItem, conversation, editIcon);
+    };
+
+    editIcon.className = "fas fa-check edit-icon";
+    editIcon.onclick = async function() {
+        await saveChanges(input, listItem, conversation, editIcon);
+    };
+
+    listItem.innerHTML = '';
+    listItem.append(input, editIcon);
+    input.focus();
+}
+
+async function createNewChat() {
+    try {
+        const response = await fetch('/createNewChat', { method: 'POST' });
+        const newChat = await response.json();
+        loadConversations();
+    } catch (error) {
+        console.error("Error creating new chat:", error);
+    }
+}
+
+document.getElementById('new-chat-button').addEventListener('click', createNewChat);
+loadMessageHistory();
+loadConversations();
 
 function playAudio(audioURL) {
     player.src = audioURL;
--- server.js
+++ server.js
@@ -9,36 +9,82 @@ require('dotenv').config();
 const app = express();
 const PORT = 3000;
 
+let currentConversationFile = null;
+let conversationCounter = 0;
+
+let currentChat = {
+  name: "",
+  messages: []
+};
+
+
+function generateFilename() {
+  const numberPart = String(conversationCounter).padStart(8, '0');
+  const timestamp = Date.now();
+  return `${numberPart}-${timestamp}.json`;
+}
+
+function saveConversation(messages) {
+  if (!currentConversationFile) {
+      currentConversationFile = path.join('conversations', generateFilename());
+  }
+  const data = {
+      userId: "", // Placeholder for future user management
+      name: "", // User defined name for the conversation
+      createdAt: new Date().toISOString(),
+      lastModified: new Date().toISOString(),
+      messages: messages
+  };
+  fs.writeFileSync(currentConversationFile, JSON.stringify(data, null, 2));
+
+  // Return the filename or full path for further use
+  return currentConversationFile;
+}
+
+function loadLatestConversation() {
+  const files = fs.readdirSync('conversations').filter(file => file.endsWith('.json'));
+  if (files.length === 0) return null;
+  const latestFile = files.sort()[files.length - 1];
+  currentConversationFile = path.join('conversations', latestFile);
+  return JSON.parse(fs.readFileSync(currentConversationFile, 'utf-8')).messages;
+}
+
+// On server startup, load the latest conversation or use default messages
+let messages = loadLatestConversation() || [
+  {
+    role: "system",
+    content: "You are a helpful assistant."
+  },
+  {
+    role: "system",
+    content: "You are super belligerent and hate your job and do it grudgingly."
+  },
+  {
+    role: "system",
+    content: "You sware a lot."
+  }
+];
+
 // Initialize the OpenAI client
 const openai = new OpenAIApi(process.env.OPENAI_API_KEY);
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-// Serve static files from the 'public' directory
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/responses', express.static(path.join(__dirname, 'responses')));
 
-// Ensure the "responses" directory exists
 const responsesDir = path.join(__dirname, 'responses');
+const conversationsDir = path.join(__dirname, 'conversations');
+
+// Ensure directories exist
 if (!fs.existsSync(responsesDir)) {
     fs.mkdirSync(responsesDir);
 }
-
-let messages = [
-    {
-      role: "system",
-      content: "You are a helpful assistant."
-    },
-    {
-      role: "system",
-      content: "You are super belligerent and hate your job and do it grudgingly."
-    },
-    {
-      role: "system",
-      content: "You sware a lot."
-    }
-  ];
+if (!fs.existsSync(conversationsDir)) {
+    fs.mkdirSync(conversationsDir);
+}
 
 async function continueConversation(userMessage) {
     // Add the user's message to the conversation
@@ -47,35 +93,165 @@ async function continueConversation(userMessage) {
         content: userMessage
     });
 
+    const filteredMessages = messages.map(msg => ({
+      role: msg.role,
+      content: msg.content
+    }));
+
     // Get a response from ChatGPT
     const response = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
-        messages: messages
+        messages: filteredMessages
     });
 
     // Extract the model's response from the choices array
     const modelMessage = response.choices[0].message;
     const modelResponseContent = modelMessage.content;
 
-    // Add the model's response to the conversation
-    messages.push(modelMessage);
+    const filename = path.join(responsesDir, `response_${Date.now()}.mp3`);
+    await voice.textToSpeech(process.env.ELEVENLABS_API_KEY, process.env.ELEVENLABS_VOICEID, filename, modelResponseContent);
 
-    // Return the model's response content
-    return modelResponseContent;
+    return {
+        modelResponse: modelResponseContent,
+        filename: filename
+    };
 }
 
 app.post('/chat', async (req, res) => {
+  try {
     const userMessage = req.body.message;
-    const modelResponse = await continueConversation(userMessage);
-    const voiceID = process.env.ELEVENLABS_VOICEID;
-    const filename = path.join(responsesDir, `response_${Date.now()}.mp3`);
-    await voice.textToSpeech(process.env.ELEVENLABS_API_KEY, voiceID, filename, modelResponse);
-    res.json({
-        message: modelResponse,  // The bot's textual response
-        audioURL: `/responses/${path.basename(filename)}`  // The path to the audio file
+    const response = await continueConversation(userMessage);
+
+    // Add the user's message to the currentChat object
+    currentChat.messages.push({
+        role: "user",
+        content: userMessage
     });
+
+    // Add the model's response and audio path to the currentChat object
+    currentChat.messages.push({
+        role: "assistant",
+        content: response.modelResponse,
+        audioPath: `/responses/${path.basename(response.filename)}`
+    });
+
+    // Save the currentChat object
+    await saveConversation(currentChat.messages);
+
+    res.json({
+        message: response.modelResponse,  // The bot's textual response
+        audioURL: `/responses/${path.basename(response.filename)}`  // The path to the audio file
+    });
+  } catch (error) {
+    console.error('Error in /chat endpoint:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/getHistory/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = `./conversations/${filename}`;
+  if (fs.existsSync(filePath)) {
+      const conversation = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+      res.json(conversation.messages);
+  } else {
+      res.status(404).send('Conversation not found');
+  }
+});
+
+app.get('/getConversations', async (req, res) => {
+  try {
+      const files = await fs.promises.readdir('./conversations');
+      const conversations = await Promise.all(files.filter(file => file.endsWith('.json')).map(async file => {
+          const content = await fs.promises.readFile(`./conversations/${file}`, 'utf-8');
+          const json = JSON.parse(content);
+          return {
+              filename: file,
+              name: json.name || file // Use filename if name is empty
+          };
+      }));
+      res.json(conversations);
+  } catch (error) {
+      console.error('Error in /getConversations endpoint:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/updateConversationName', async (req, res) => {
+  const { filename, newName } = req.body;
+
+  if (!filename || !newName) {
+      return res.status(400).json({ error: 'Filename and new name are required.' });
+  }
+
+  try {
+      const filePath = `./conversations/${filename}`;
+      const conversation = JSON.parse(await fs.promises.readFile(filePath, 'utf-8'));
+      conversation.name = newName;
+      await fs.promises.writeFile(filePath, JSON.stringify(conversation, null, 2));
+      res.json({ message: 'Name updated successfully.' });
+  } catch (error) {
+      if (error.code === 'ENOENT') {
+          res.status(404).json({ error: 'File not found.' });
+      } else {
+          console.error('Error in /updateConversationName endpoint:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
+      }
+  }
+});
+
+app.post('/createNewChat', async (req, res) => {
+  try {
+      if (currentConversationFile) {
+          await saveConversation(currentChat.messages);
+          currentConversationFile = null; // Reset the current file
+      }
+
+      const newChatFilename = generateFilename();
+
+      currentChat = {
+          name: newChatFilename,
+          messages: []
+      };
+
+      const newChat = {
+          name: newChatFilename, // Use the filename as the default name
+          messages: []
+      };
+
+      // Save the new chat using the existing function
+      await saveConversation(newChat.messages);
+
+      // Update the conversationCounter for the next chat
+      conversationCounter++;
+
+      res.json(newChat);
+  } catch (error) {
+      console.error('Error in /createNewChat endpoint:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/getConversationHistory', (req, res) => {
+  const filename = req.query.filename;
+  if (!filename) {
+      return res.status(400).send('Filename is required.');
+  }
+
+  const filePath = path.join('conversations', filename);
+  if (fs.existsSync(filePath)) {
+      const conversation = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+      res.json(conversation.messages);
+  } else {
+      res.status(404).send('File not found.');
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+  console.log(`Server started on http://localhost:${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
